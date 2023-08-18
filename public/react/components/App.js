6a1aaf59bf266c374636c5c4edd3db567b005884
Frank Tennant <frank.tennant@wmg.com>
2023-08-18 16:15:01 +0100

    initial commit

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,32 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import { PagesList } from './PagesList';
+import React, { useState, useEffect } from "react";
 
 // import and prepend the api url to any fetch calls
-import apiURL from '../api';
+import apiURL from "../api";
 
 export const App = () => {
+  const [pages, setPages] = useState([]);
+  const [article, setArticle] = useState(null);
 
-	const [pages, setPages] = useState([]);
+  async function fetchArticle(slug) {
+    try {
+      const response = await fetch(`${apiURL}/wiki/${slug}`);
+      const articleData = await response.json();
+      setArticle(articleData);
+    } catch (err) {
+      console.log("Oh no an error! ", err);
+    }
+  }
 
-	async function fetchPages(){
-		try {
-			const response = await fetch(`${apiURL}/wiki`);
-			const pagesData = await response.json();
-			setPages(pagesData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
+  useEffect(() => {
+    async function fetchPages() {
+      try {
+        const response = await fetch(`${apiURL}/wiki`);
+        const pagesData = await response.json();
+        setPages(pagesData);
+      } catch (err) {
+        console.log("Oh no an error! ", err);
+      }
+    }
 
-	useEffect(() => {
-		fetchPages();
-	}, []);
+    fetchPages();
+  }, []);
 
-	return (
-		<main>	
-      <h1>WikiVerse</h1>
-			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
+  if (article) {
+    return (
+		<main>
+			<a href="#" onClick={function (event) {
+              event.preventDefault();
+			  setArticle(null);
+            }}>&larr; Home</a>
+			<h1>{article.title}</h1>
+			<p><b>Author:</b></p>
+			<p><b>Published:</b> </p>
+			<p>{article.content}</p>
+			<p><b>Tags:</b></p>
+			
 		</main>
-	)
-}
+	);
+  }
+
+  return (
+    <main>
+      <h1>WikiVerse</h1>
+      <h2>An interesting 📚</h2>
+      {pages.map((page) => (
+        <h3 key={page.id}>
+          <a
+            href="#"
+            onClick={function (event) {
+              event.preventDefault();
+			  fetchArticle(page.slug);
+            }}
+          >
+            {page.title}
+          </a>
+        </h3>
+      ))}
+    </main>
+  );
+};
